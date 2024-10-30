30ebdf01c52274fc617f09eea8d0cb1e3e403c73
Christian0205 <Christiang2605@gmail.com>
2024-10-30 13:01:30 -0500

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,60 @@
+import java.util.ArrayList;
+
+interface Ingredient{
+    public String getName();
+    public double getQuantity();
+
+
+}
+class SolidIngredient implements Ingredient{
+    String _ingredientName;
+    double _quantity;
+    public SolidIngredient(String x, double y){
+        _ingredientName = x;
+        _quantity = y;
+    }
+    public String getName(){
+        return _ingredientName;
+    }
+    public double getQuantity(){
+        return _quantity;
+    }
+
+
+}
+class liquidIngredient implements Ingredient{
+    String _ingredientName;
+    double _quantity;
+    public liquidIngredient(String x, double y){
+        _ingredientName = x;
+        _quantity = y;
+    }
+    public String getName(){
+        return _ingredientName;
+    }
+    public double getQuantity(){
+        return _quantity;
+    }
+
+}
+
+class Recipe<T extends Ingredient>{
+    private String name;
+    private String instructions;
+    private ArrayList<T> _ingredients;
+    public Recipe(int size){_ingredients = new ArrayList<T>();}
+
+    public void addIngredient(Ingredient t){
+        _ingredients.add((T) t);
+    }
+
+
+}
+
+
+
+
+
 public class Main {
     public static void main(String[] args) {
 
