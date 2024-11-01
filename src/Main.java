39830abf014dfa6b2f3b2968bc9422e2ade64afa
Christian0205 <Christiang2605@gmail.com>
2024-11-01 13:04:15 -0500

    Revision 4

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 interface Ingredient{
-    public String getName();
-    public double getQuantity();
+    String getName();
+    double getQuantity();
 
 
 }
 class SolidIngredient implements Ingredient{
-    String _ingredientName;
-    double _quantity;
+    private String _ingredientName;
+    private double _quantity;
     public SolidIngredient(String x, double y){
         _ingredientName = x;
         _quantity = y;
@@ -23,36 +25,57 @@ class SolidIngredient implements Ingredient{
 
 }
 class liquidIngredient implements Ingredient{
-    String _ingredientName;
-    double _quantity;
+    private String _ingredientName;
+    private double _quantityMl;
     public liquidIngredient(String x, double y){
         _ingredientName = x;
-        _quantity = y;
+        _quantityMl = y;
     }
     public String getName(){
         return _ingredientName;
     }
     public double getQuantity(){
-        return _quantity;
+        return _quantityMl;
     }
 
 }
 
 class Recipe<T extends Ingredient>{
+    //whatever you pass in, the data type of that has to implement the interface(Constraint)
     private String _name;
     private String _instructions;
     private ArrayList<T> _ingredients;
-    public Recipe(String name, String instructions, int size){_name = name;
+    public Recipe(String name, String instructions){
+        _name = name;
         _ingredients = new ArrayList<T>();
         _instructions = instructions;}
 
-    public void addIngredient(Ingredient t){
-        _ingredients.add((T) t);
+    public void addIngredient(T t){
+        _ingredients.add(t);
+    }
+    public static void addIngredient(Recipe<Ingredient> recipe, Scanner s){
+        System.out.println("Is this a solid (s) or a liquid (l)");
+        char type = s.nextLine().charAt(0);
+        System.out.println("Enter new ingredient name");
+        String name = s.nextLine();
+        System.out.println("Enter the quantity");
+        double quantity = Double.parseDouble(s.nextLine());
+        Ingredient ingredient = null;
+        if(type == 's'){
+            ingredient = new SolidIngredient(name,quantity);
+        }if(type == 'l'){
+            ingredient = new liquidIngredient(name,quantity);
+        }
+        recipe.addIngredient(ingredient);
     }
     public void print(){
-        if ( _name!= null){
-            System.out.println("Name:" + _name.getClass().getName());
+        System.out.println("Name:" + _name);
+        System.out.println("Instructions:" + _instructions);
+        System.out.println("Ingredients:");
+        for(T t:_ingredients){
+            System.out.println(t.getName() + "-" + t.getQuantity());
         }
+
     }
 
 
@@ -64,6 +87,16 @@ class Recipe<T extends Ingredient>{
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("1.Add Ingredient \n 2. List the ingredients for a recipe \n 3. Exit  ");
+        Recipe<Ingredient> pie = new Recipe<>("Pie", "Bake");
+        Recipe<Ingredient> x;
+        Scanner s = new Scanner(System.in);
+        s.nextInt();
+        while(s.nextInt() != 3){
+            if(s.nextInt() == 1){
+                
+            }
+        }
 
     }
 }
